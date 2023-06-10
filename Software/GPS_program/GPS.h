27ad55a147202de43735787194dc40ca939ff6f1
Eric Creasy <36.creasy@gmail.com>
2023-06-10 13:49:56 -0600

    implemented most functions for main boards -functions and logic implemented for GPS -functions and logic added for PDB -functions and logic added for Radio -functions and logic added for recovery board

--- Software/GPS_program/GPS.h
+++ Software/GPS_program/GPS.h
@@ -4,4 +4,8 @@ private:
     /* data */
 public:
     GPS();
+
+    bool has_input();
+
+    void main();
 };
--- Software/flight_computer_program/SensorData.h
+++ Software/flight_computer_program/SensorData.h
@@ -1,13 +1,19 @@
 #pragma once
 class SensorData{
+private:
+
 public:
     SensorData();
 
     void setData(SensorData data);
     //SensorData - needs to store data from the following sensors:
-    //    - accelerometer
-    //    - gyroscope
-    //    - magnetometer
-    //    - barometer
+    //    - accelerometer -kx134
+    //    - gyroscope 6axis includes accelerometer -bmi088
+    //    - magnetometer -NA
+    //    - find one which does all 3 of above sensors
+    //    - barometer -bmp280
+    //    - thermocouple - MAX31855
+
+    //    - flight state
 
 };
